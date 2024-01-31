03cb424a4c21baf37286f4d6cd46e5007f54a126
Hema <5p7fuupnnqaao3sjgyqrnwwrqvgkuxwclmef3xon7q7r6k6nbuma>
2024-01-31 14:14:14 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container-fluid">
+    <NavBar/>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
+    Home
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="container">
+  <h2>List Users</h2>
+  <table class="table">
+    <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Age</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="user in users" :key="user.id">
+      <th scope="row">{{ user.id }}</th>
+      <td>{{ user.name }}</td>
+      <td>{{ user.age }}</td>
+    </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HOME",
+  data (){
+    return {
+      users :[
+          {
+            id: 1,
+            name: "Ibrahim Samir",
+            age: 35
+          },
+          {
+            id: 2,
+            name: "Ahmed samir",
+            age: 28
+          },
+          {
+            id: 3,
+            name: "Yasin Ibrahim",
+            age: 10
+          },
+        {
+          id: 4,
+          name: "Mohammed",
+          age: 4
+        },
+        {
+          id: 5,
+          name: "Ysaaer",
+          age: 20
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
